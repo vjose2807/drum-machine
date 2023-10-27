ef3b38cbfe2b9bbf8651121f48b5343f45421401
vjose2807 <vjose2807@gmail.com>
2023-10-26 20:45:32 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const audioClips = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+  },
+];
+
+const DrumPad = ({ clip, onClick, isActive }) => (
+  <div
+    className={`drum-pad ${isActive ? 'active' : ''}`}
+    id={clip.id}
+    onClick={() => onClick(clip)}
+  >
+    {clip.keyTrigger}
+    <audio className="clip" id={clip.keyTrigger} src={clip.url}></audio>
+  </div>
+);
+
+const App = () => {
+  const [display, setDisplay] = useState('');
+
+  const playSound = (clip) => {
+    const audioElement = document.getElementById(clip.keyTrigger);
+    audioElement.play();
+    setDisplay(clip.id);
+  };
+
+  const keyPressHandler = (event) => {
+    const clip = audioClips.find((clip) => clip.keyCode === event.keyCode);
+    if (clip) {
+      playSound(clip);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', keyPressHandler);
+    return () => {
+      document.removeEventListener('keydown', keyPressHandler);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <div id="display">{display}</div>
+      <div className="drum-pads">
+        {audioClips.map((clip) => (
+          <DrumPad
+            key={clip.id}
+            clip={clip}
+            onClick={playSound}
+            isActive={display === clip.id}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
